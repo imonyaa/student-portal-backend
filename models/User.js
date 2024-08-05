5843e711ec4d2e48f3752faaa0136fbaa67e92ec
Imane Otmanine <otmanine.imane@gmail.com>
2024-08-05 13:16:12 +0100

    added default profileImage and made it not required

--- models/User.js
+++ models/User.js
@@ -57,7 +57,8 @@ const UserSchema = new mongoose.Schema({
   },
   profileImage: {
     type: String,
-    required: true,
+    default:
+      "https://as2.ftcdn.net/v2/jpg/04/10/43/77/1000_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta.jpg",
   },
 });
 
